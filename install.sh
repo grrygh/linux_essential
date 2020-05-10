085d3fbdd381c8c08741a5858eef4a68663c65b0
grrygh <42380969+grrygh@users.noreply.github.com>
2020-05-10 19:43:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-wget -nc https://raw.githubusercontent.com/grrygh/linux_essential/master/.vimrc -P ~/
+wget -c https://raw.githubusercontent.com/grrygh/linux_essential/master/.vimrc -P ~/
