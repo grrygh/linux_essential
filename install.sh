22af94518409d317072f5aff7b47ebc0294c50b0
grrygh <42380969+grrygh@users.noreply.github.com>
2020-05-14 12:58:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,22 @@
 
 #This script is for raspbian OS.
 
+# Check for root access.
+if [[ "${UID}" -ne 0 ]]
+then
+  echo 'Please run the script with sudo.' >&2
+  exit 1
+fi
+
 # Check if VIM is installed.
 which vim
 if [[ $? -ne 0 ]]
 then
   echo 'Installing VIM...'
-  sudo apt-get install -y vim
+  apt-get install -y vim
 else
   echo 'Check for VIM update...'
-  sudo apt-get update && sudo apt-get install --only-upgrade vim
+  apt-get update && sudo apt-get install --only-upgrade vim
 fi
 
 # Set default editor to VIM.
