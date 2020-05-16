c80e551d85b92e70e1e5fafdb68a92cc5b7ba416
grrygh <42380969+grrygh@users.noreply.github.com>
2020-05-16 09:00:23 +0800

    Update pihole_gravity.sh

--- pihole_gravity.sh
+++ pihole_gravity.sh
@@ -7,3 +7,11 @@ rsync -a pi@10.1.1.10:/etc/pihole/gravity.db /home/pi/etc-pihole/
 docker exec -it pihole pihole restartdns reload-list
 docker exec -it pihole pihole -g
 echo "$(date)" > pihole_gravity.log
+
+#write out current crontab
+crontab -l > mycron
+#echo new cron into cron file
+echo "0 8 * * * bash /home/pi/pihole_gravity.sh" >> mycron
+#install new cron file
+crontab mycron
+rm mycron
