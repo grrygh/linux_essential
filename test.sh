1af3d395894cee9077daa800394d5bf01ca4a7f0
grrygh <42380969+grrygh@users.noreply.github.com>
2020-05-16 08:43:19 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,10 +3,11 @@
 usage() {
   # Display the usage and exit.
   echo
-  echo "Usage: ${0} [-vd]"
+  echo "Usage: ${0} [-vdp]"
   echo 'Linux essential installation'
-  echo '  -v  Install latest VIM & setup .vimrc'
-  echo '  -d  Check if docker is install.'
+  echo '  -v  Install VIM application & setup .vimrc'
+  echo '  -d  Install Docker application.'
+  echo '  -p. Install pihole docker.'
   echo '*If curling from github, add | bash -s -- [-vd]'
   exit 1
 }
@@ -17,6 +18,7 @@ do
   case ${OPTION} in
     v) VIM='true' ;;
     d) DOCKER='true' ;;
+    p) PIHOLE='true' ;;
     ?) usage ;;
   esac
 done
@@ -27,16 +29,16 @@ then
   usage >&2
 fi
 
-# Check if VIM is install & Update.
+# Install VIM application & setup .vimrc
 if [[ "${VIM}" = 'true' ]]
 then
-  echo 'Display from VIM section.'
-  which vim
-  if [[ $? -ne 0 ]]
+  which vim # Check if VIM is installed.
+  if [[ $? -ne 0 ]] # If VIM is not installed.
   then
     echo 'Installing VIM...'
+    sudo apt-get update && sudo apt-get upgrade
     sudo apt-get install -y vim
-  else
+  else # If VIM is installed, check for update.
     echo 'Check for VIM update...'
     sudo apt-get update && sudo apt-get install --only-upgrade vim
   fi
@@ -57,10 +59,26 @@ then
   wget https://raw.githubusercontent.com/grrygh/linux_essential/master/.vimrc -P ~/
 fi
 
-# Check if Docker is install & update.
+# Install Docker application.
 if [[ "${DOCKER}" = 'true' ]]
 then
-  echo 'Display from Docker section.'
+  which docker # Check if Docker is installed.
+  if [[ $? -ne 0 ]] # If Docker is not installed.
+  then
+    echo 'Installing Docker...'
+    sudo apt-get update && sudo apt-get upgrade
+    cd ~
+    curl -fsSL https://get.docker.com -o get-docker.sh # Download docker install script.
+    sudo sh get-docker.sh
+    sudo usermod -aG docker pi # Add pi to Docker group.
+    # Install Docker compose.
+    sudo apt-get install libffi-dev libssl-dev
+    sudo apt install python3-dev
+    sudo apt-get install -y python3 python3-pip
+    sudo pip3 install docker-compose
+  else
+    echo 'Docker is already installed.'
+  fi
 fi
 
 exit 0
