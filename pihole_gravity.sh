60df4b85775d92f41f65f29d6f364b9cbd65a314
grrygh <42380969+grrygh@users.noreply.github.com>
2020-05-16 09:01:24 +0800

    Update pihole_gravity.sh

--- pihole_gravity.sh
+++ pihole_gravity.sh
@@ -3,6 +3,7 @@
 # This script is to poll gravity.db from pihole production server
 # to this host docker pihole.
 
+# Rsync from production pihole.
 rsync -a pi@10.1.1.10:/etc/pihole/gravity.db /home/pi/etc-pihole/
 docker exec -it pihole pihole restartdns reload-list
 docker exec -it pihole pihole -g
