e163c3cc4c2cef3ad08fefffd4a2bb71a3654c35
Grry Gh <gohgarry@gmail.com>
2020-05-25 14:53:35 +0800

    commit

--- install.sh
+++ install.sh
@@ -97,6 +97,8 @@ then
       docker-compose -f pihole.yml up -d pihole # Start pihole container.
       wget https://raw.githubusercontent.com/grrygh/linux_essential/master/pihole_gravity.sh -P ~/
     fi
+  else
+    echo 'pihole container is running'
   fi
 fi
 
