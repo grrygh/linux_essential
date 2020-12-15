fbfa127ce838926828affdf8669fd5c7c31026d7
grrygh <42380969+grrygh@users.noreply.github.com>
2020-12-15 15:49:43 +0800

    Update

--- install.sh
+++ install.sh
@@ -54,14 +54,12 @@ fi
 
 if [[ "${LOCALE}" = 'true' ]]
 then
-
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 echo 'export LC_ALL=en_US.UTF-8' >> ~/.bashrc
 echo 'export LANG=en_US.UTF-8' >> ~/.bashrc
 echo 'export LANGUAGE=en_US.UTF-8' >> ~/.bashrc
-
 fi
 
 # Install Mosh server.
